93c206557dbdd80c38c2e72a60412b73dab929eb
ChrisvanHvA <Chris.gabriel.iii@hva.nl>
2022-04-20 14:09:05 +0200

    recovery

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const app = express()
+const port = 4000
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs', 'html');
+
+app.get('/', function(req, res) {
+    
+    res.render('index.ejs', {
+     
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`web server running on http://localhost:${port}}`)
+})
