e739a5a5502450899e01219098f28f599caae1b1
Laiba Choudhry <laiba@hotmail.nl>
2022-04-21 13:32:41 +0200

    deployen

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const port = process.env.PORT || 4000;
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs', 'html');
 
+app.use(express.static("public"));
+
+
+
 app.get('/', function(req, res) {
     
     res.render('index.ejs', {
      
     });
   });
+ 
+
+
 
 app.listen(port, () => {
   console.log(`web server running on http://localhost:${port}}`)
