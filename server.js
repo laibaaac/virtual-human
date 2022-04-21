a96a2d7b7a28f17b67788e21559b01ffbbda0925
Laiba Choudhry <laiba@hotmail.nl>
2022-04-21 14:50:03 +0200

    set up compleet

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = process.env.PORT ||4000
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs', 'html');
 
@@ -11,6 +12,25 @@ app.get('/', function(req, res) {
      
     });
   });
+
+
+  app.get("/start", async (req, res) => {
+  
+    res.render("start");
+  
+  });
+
+
+  app.get("/list", async (req, res) => {
+  
+    res.render("list");
+  
+  });
+
+  app.use(express.static("public"));
+
+
+
 
 app.listen(port, () => {
   console.log(`web server running on http://localhost:${port}}`)
