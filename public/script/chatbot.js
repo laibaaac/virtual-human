c7a12fdd461e21c6f7b3077755307c860bc0eb9a
ChrisvanHvA <Chris.gabriel.iii@hva.nl>
2022-04-29 12:49:05 +0200

    code update

--- public/script/chatbot.js
+++ public/script/chatbot.js
@@ -1,3 +1,5 @@
+window.onload=function(){
+
 
 document.getElementById("smileButton").addEventListener("click", smileCode);
 document.getElementById("frowningButton").addEventListener("click", frownCode);
@@ -8,6 +10,7 @@ document.getElementById("lookLeftButton").addEventListener("click", lookLeftCode
 document.getElementById("lookRightButton").addEventListener("click", lookRightCode);
 document.getElementById("lookDownButton").addEventListener("click", lookDownCode);
 document.getElementById("eyebrowsRaiseButton").addEventListener("click", eyebrowRaiseCode);
+document.getElementById("eyebrowsRaiseButton").addEventListener("click",eyebrowNumber);
 document.getElementById("eyebrowsSmallButton").addEventListener("click", eyebrowsSmallCode);
 document.getElementById("frownSmallButton").addEventListener("click", frownSmallCode);
 document.getElementById("nodButton").addEventListener("click", nodCode);
@@ -18,10 +21,12 @@ document.getElementById("questionButton").addEventListener("click", questionCode
 document.getElementById("friendlyButton").addEventListener("click", friendlyCode);
 document.getElementById("agreeButton").addEventListener("click", agreeCode);
 document.getElementById("disagreeButton").addEventListener("click", disagreeCode);
-
-var dura=0;
+document.getElementById("plusOne").addEventListener("click", addOne);
+document.getElementById("minusOne").addEventListener("click", removeOne);
+var dura=4;
 
 function smileCode() {
+
     document.getElementById("textReturn").innerHTML = "<trl-anim type='aux' id='smileLarge' duration='3.5' blendStart='0.5' blendEnd='0.5' peak='0.8'/>";
   }
 
@@ -31,6 +36,7 @@ function frownCode() {
   }
 
   function frownSmallCode() {
+    
     document.getElementById("textReturn").innerHTML = "<trl-anim type='aux' id='frownSmall' duration='3.5' blendStart='0.5' blendEnd='0.5'/>";
   }
   function mouthCode() {
@@ -51,10 +57,15 @@ function frownCode() {
   function lookDownCode() {
     document.getElementById("textReturn").innerHTML = "<trl-config lookat='0,0,-20' enable='true' speed='3.75' wait='2.5' returnSpeed='5.25' absolute='false' /><trl-anim type='aux' id='tiltDownLarge3' duration='2.5' />";
   }
+  function eyebrowNumber() {
+    dura=4;
+    console.log(dura);
+  }
   function eyebrowRaiseCode() {
-dura=4;
+
     document.getElementById("textReturn").innerHTML = "<trl-anim type='aux' id='raisedBrowLarge' duration=" + parseInt(dura) + " blendStart='1.5' blendEnd='1.5'/>";
   }
+  
   function eyebrowsSmallCode() {
     document.getElementById("textReturn").innerHTML = "<trl-anim type='aux' id='raisedBrowSmall' duration='3.5' blendStart='0.5' blendEnd='0.5'/>";
   }
@@ -84,9 +95,27 @@ dura=4;
   function disagreeCode() {
     document.getElementById("textReturn").innerHTML = "<trl-anim type='aux' id='frownLarge' duration='4.5' blendStart='1.5' blendEnd='1.5'/>";
   }
+ 
+  function addOne() {
+    dura += 1;
+    console.log(dura);
+    eyebrowRaiseCode();
+    }
+   
+
+
+  function removeOne() {
+    dura -= 1;
+
+    console.log(dura);
+    eyebrowRaiseCode();
+  }
+
+
 
   for (var i = 0; i < document.links.length; i++) {
     if (document.links[i].href === document.URL) {
         document.links[i].className = 'current';
     }
  }
+}
