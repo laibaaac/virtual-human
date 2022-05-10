cc18b3a5443397d962de3b3fc0839e09b1930e0f
Laiba Choudhry <laiba@hotmail.nl>
2022-05-10 16:00:41 +0200

    filteruuhhhhhh

--- public/script/filter.js
+++ public/script/filter.js
@@ -1,27 +1,50 @@
-var buttonAll = document.querySelector("#filter-all");
-var buttonPeople = document.querySelector("#filter-eyes");
-var buttonAnimals = document.querySelector("#filter-mouth");
-
-
-function filterList(event){
-  /* de ul opzoeken */
-  let deLijst = document.querySelector("ul");
-  let nieuweFilter = event.target.value;
-
-  deLijst.className = "";
-
-  deLijst.classList.add(nieuweFilter);
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  //if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
 }
 
-/* de radio buttons laten luisteren naar wijzigingen */
-/* dan de functie filterList uitvoeren */
-buttonAll.addEventListener("change", filterList);
-buttonEyes.addEventListener("change", filterList);
-buttonMouth.addEventListener("change", filterList);
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
 
-
-
-
-/*document.querySelectorAll('div').forEach(target => target.classList.remove("showcommands"));
-document.getElementById("eyecommands").classList.add("showcommands");*/
+//bron van code = w3school.com -- https://www.w3schools.com/howto/howto_js_filter_elements.asp//
