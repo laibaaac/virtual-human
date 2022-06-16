6276b1bb54306e0252aabe5525252d472e93b4dc
Laiba Choudhry <laiba@hotmail.nl>
2022-06-16 11:50:48 +0200

    kijk even

--- server.js
+++ server.js
@@ -1,78 +1,67 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
-const port = process.env.PORT ||4000
+const port = process.env.PORT || 4000;
 bodyParser = require("body-parser");
-const auth = require('basic-auth');
-const compare = require('tsscmp'); 
+
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs", "html");
+
+const auth = require("basic-auth");
+const compare = require("tsscmp");
+
+const check = (pass) => {
+  let valid = true;
+
+  valid = compare(pass, "0000") && valid;
+  return valid;
+};
+
+const basicAuth = (req, res, next) => {
+  const credentials = auth(req);
+  if (credentials && check(credentials.pass)) {
+    return next();
+  }
+
+  res.set("WWW-authenticate", 'Basic realm="someSite"');
+  return res.status(401).send("unauthorized");
+};
+
+app.use(basicAuth);
+
+app.get("/", function (req, res) {
 
 
-
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs', 'html');
-
-app.get('/', function(req, res) {
-  
-//const password = "0000";
-
-/*function passcheck() {
-    if(document.getElementById('pass1').value !=password){
-        alert('Wrong password, Try again.')
-        return false;
-        return next();
-    }
-    if(document.getElementById('pass1').value ==password){
-        res.redirect('/dashboard');
-    }
-*/ 
-
-
-  res.render('index', {
-  });
- 
-}
-
-
-);
-
-app.get("/dashboard", async (req, res) => {
-  
-  res.render("dashboard");
-
+  res.render("index", {});
 });
 
-  app.get("/start", async (req, res) => {
-  
-    res.render("start");
-  
-  });
+
+app.get("/dashboard", async (req, res) => {
+  res.render("dashboard");
+});
 
 
-  app.get("/list", async (req, res) => {
-  
-    res.render("list");
-  
-  });
-
- 
+app.get("/start", async (req, res) => {
+  res.render("start");
+});
 
 
-  app.get("/avatar", async (req, res) => {
-  
-    res.render("avatar");
-  
-  });
-  app.get("/avataar", async (req, res) => {
-  
-    res.render("avataar");
-  
-  });
-  app.use(express.static("public"));
+app.get("/list", async (req, res) => {
+  res.render("list");
+});
 
 
+app.get("/avatar", async (req, res) => {
+  res.render("avatar");
+});
+
+
+app.get("/avataar", async (req, res) => {
+  res.render("avataar");
+});
+app.use(express.static("public"));
 
 
 app.listen(port, () => {
-  console.log(`web server running on http://localhost:${port}}`)
-})
+  console.log(`web server running on http://localhost:${port}}`);
+});
