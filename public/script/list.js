c0405bfafebc20131447a1a9feb23a4c62ccd70c
ChrisvanHvA <Chris.gabriel.iii@hva.nl>
2022-06-17 15:32:14 +0200

    tts updates

--- public/script/list.js
+++ public/script/list.js
@@ -60,9 +60,6 @@ document.getElementById("resetButton").addEventListener('click', () => {
 //Get the button:
 mybutton = document.getElementById("B2TBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
