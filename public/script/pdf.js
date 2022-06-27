11233ce54be5cd999d75a7b491c205b75b71ef5a
Laiba Choudhry <laiba@hotmail.nl>
2022-06-27 15:50:51 +0200

    oke is nu erin

--- public/script/pdf.js
+++ public/script/pdf.js
@@ -1,4 +1,22 @@
-var pdfmake = require('pdfmake');
+const pdfmake = window.pdfMake;
+const PDFButton = document.getElementById("pdftje");
+
+function pdfButton() {
+  var dd = { content: document.getElementById("plain-textarea-id").value };
+  pdfmake.createPdf(dd).download();
+}
+
+// Add an event listener on CopyButton, when clicked copy the value from #plain-textarea-id and log the value to the console
+PDFButton.addEventListener("click", function () {
+  var copyText = document.getElementById("plain-textarea-id");
+  console.log(copyText.value);
+  pdfButton();
+});
+
+
+
+
+/*var pdfmake = require('pdfmake');
 
 var pdfContent = document.getElementById("plain-textarea-id");
 document.getElementById('pdftje').addEventlistener("click", pdfButton)
@@ -16,7 +34,7 @@ function pdfButton() {
     }, err => {
       console.error(err);
     });  
-}
+}*/
 
 
 
