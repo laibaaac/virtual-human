1f541d4dbbe7f6a812a02dc98b08012383a4a59f
ChrisvanHvA <Chris.gabriel.iii@hva.nl>
2022-06-30 15:28:36 +0200

    clipboard fix

--- public/script/list.js
+++ public/script/list.js
@@ -50,34 +50,3 @@ document.getElementById("resetButton").addEventListener('click', () => {
     timelineImg.remove();
   })})
 
-if(navigator.clipboard) {
-  
-
-  const codeSnippet = document.querySelectorAll('li textarea')
-  
-  codeSnippet.forEach((element, index) => {  
-    element.addEventListener('click', () =>{
-    setClipboard(element.textContent)
-    })
-  })
-  
-  function setClipboard(text) {
-
-      const type = "text/plain";
-      const blob = new Blob([text], { type });
-      const data = [new ClipboardItem({ [type]: blob })];
-  
-      navigator.clipboard.write(data).then(
-          function () {
-          /* success */
-            // console.log(data)
-          },
-          function () {
-          /* failure */
-            // console.log('error clipping')
-          }
-      );
-  
-  }
-  }
-
