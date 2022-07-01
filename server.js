e5defa5e162b550ecbc4900230d5ecca67d56498
Laiba Choudhry <laiba@hotmail.nl>
2022-07-01 17:35:00 +0200

    login erbij

--- server.js
+++ server.js
@@ -7,6 +7,27 @@ bodyParser = require("body-parser");
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs", "html");
 
+const auth = require("basic-auth");
+const compare = require("tsscmp");
+
+const check = (pass) => {
+  let valid = true;
+
+  valid = compare(pass, "0000") && valid;
+  return valid;
+};
+
+const basicAuth = (req, res, next) => {
+  const credentials = auth(req);
+  if (credentials && check(credentials.pass)) {
+    return next();
+  }
+
+  res.set("WWW-authenticate", 'Basic realm="someSite"');
+  return res.status(401).send("unauthorized");
+};
+
+app.use(basicAuth);
 
 app.get("/", function (req, res) {
 
