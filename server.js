3393e6b6298541e5c65032fabbf34b32c5a0354b
ChrisvanHvA <Chris.gabriel.iii@hva.nl>
2022-07-03 23:58:50 +0200

    heroku push

--- server.js
+++ server.js
@@ -7,33 +7,9 @@ bodyParser = require("body-parser");
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs", "html");
 
-const auth = require("basic-auth");
-const compare = require("tsscmp");
-
-const check = (pass) => {
-  let valid = true;
-
-  valid = compare(pass, "0000") && valid;
-  return valid;
-};
-
-const basicAuth = (req, res, next) => {
-  const credentials = auth(req);
-  if (credentials && check(credentials.pass)) {
-    return next();
-  }
-
-  res.set("WWW-authenticate", 'Basic realm="someSite"');
-  return res.status(401).send("unauthorized");
-};
-
-app.use(basicAuth);
-
-app.get("/", function (req, res) {
-
 
   res.render("index", {});
-});
+
 
 
 app.get("/start", async (req, res) => {
